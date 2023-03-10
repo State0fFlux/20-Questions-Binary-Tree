c7382a6746d59e8b09788edc8285d74a46a38027
State0fFlux <127552262+State0fFlux@users.noreply.github.com>
2023-03-10 13:08:26 -0800

    Add files via upload

---
+++ QuestionMain.java
@@ -0,0 +1,139 @@
+/* Brady Manske
+ * 3/10/23
+ * CS&145
+ * 20 Questions (4 of 4)
+ *
+ * CHANGELOG:
+ *     Updated nextBoolean to recurse itself until
+ *     user enters a yes/no answer. This way, if the user accidentally
+ *     hits enter before typing a response (or types an invalid response
+ *     like "maybe", the program won't assume the user said "no", and
+ *     it will instead continue searching for the boolean.
+ *
+ *
+ * To use the jGRASP debugger with this program, set a breakpoint
+ * and once the execution breaks, open 'this' or 'tq' on the left,
+ * then look at its variable 'tree'.  That's your QuestionTree.
+ * Drag your 'tree' over to the right to see a visualization of it.
+ * 
+ * (Your QuestionTree is constructed by this file on line 30.
+ * The overall loop to play games is around line 68.)
+ */
+
+import java.io.*;
+import java.util.Scanner;
+
+/** A basic text user interface for the 20 questions game. */
+public class QuestionMain implements UserInterface {
+    public static void main(String[] args) {
+        QuestionMain tq = new QuestionMain();
+        tq.run();
+    }
+    
+    
+    // fields
+    private Scanner console;
+    private QuestionTree tree;
+    
+    /** Constructs a text user interface and its question tree. */
+    public QuestionMain() {
+        console = new Scanner(System.in);
+        tree = new QuestionTree(this);
+    }
+    
+    /**
+     * Returns the user's response as a String.
+     */
+    public String nextLine() {
+        return console.nextLine();
+    }
+
+    /** Prints the given string to the console. */
+    public void print(String message) {
+        System.out.print(message);
+        System.out.print(" ");
+    }
+
+    /** Prints the given string to the console. */
+    public void println(String message) {
+        System.out.println(message);
+    }
+
+    /** Prints a blank line to the console. */
+    public void println() {
+        System.out.println();
+    }
+
+    /**
+     * Pre: None
+     * Post:
+     * - Continues prompting the user for input until they respond
+     *   with ["y"]es or ["n"]o  (can also be yeah or nah, so long as
+     *   it starts with "y"/"n")
+     * - Returns the user's response as a boolean ("y" -> true, "n" -> false)
+     */
+    public boolean nextBoolean() {
+        String answer = console.nextLine().trim().toLowerCase();
+        boolean yesOrNo = answer.startsWith("y");
+        if (answer.length() < 1 || (!answer.startsWith("y")
+            && !answer.startsWith("n"))) {
+        // recursive case, user did not give a yes/no response
+            yesOrNo = nextBoolean();
+        } // end of if
+        return yesOrNo;
+    } // end of nextBoolean method
+    
+    // private helper for overall game(s) loop
+    private void run() {
+        println("Welcome to the game of 20 Questions!");
+        load();
+        
+        // "Think of an item, and I will guess it in N tries."
+        println("\n" + BANNER_MESSAGE);
+            
+        do {
+            // play one complete game
+            println();      // blank line between games
+            tree.play();
+            print(PLAY_AGAIN_MESSAGE);
+        } while (nextBoolean());   // prompt to play again
+        
+        // print overall stats
+        // Games played: N ...  I have won: M
+        println("\n" + String.format(STATUS_MESSAGE,
+                tree.totalGames(), tree.gamesWon()));
+
+        save();
+    }
+    
+    // common code for asking the user whether they want to save or load
+    private void load() {
+        print(LOAD_MESSAGE);
+        if (nextBoolean()) {
+            print(SAVE_LOAD_FILENAME_MESSAGE);
+            String filename = nextLine();
+            try {
+                Scanner in = new Scanner(new File(filename));
+                tree.load(in);
+            } catch (FileNotFoundException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    }
+    
+    // common code for asking the user whether they want to save or load
+    private void save() {
+        print(SAVE_MESSAGE);
+        if (nextBoolean()) {
+            print(SAVE_LOAD_FILENAME_MESSAGE);
+            String filename = nextLine();
+            try {
+                PrintStream out = new PrintStream(new File(filename));
+                tree.save(out);
+                out.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    }
+}
---
+++ QuestionNode.java
@@ -0,0 +1,118 @@
+/**
+ * Brady Manske
+ * 3/10/23
+ * CS&145
+ * 20 Questions (1 of 4)
+ *
+ * This defines a node for a binary search tree
+ * that can be used in a game of 20 questions
+ *
+ * EXTRA CREDIT (?):
+ *     Fields are encapsulated! Instead of leaving
+ *     fields public, I instead provided
+ *     public get and set methods for each field.
+ */
+
+public class QuestionNode {
+    // declaring fields
+    private String data;
+    private QuestionNode left;
+    private QuestionNode right;
+    
+    /**
+     * Pre:
+     * - String parameter must not be null
+     * Post:
+     * - Constructs a new QuestionNode storing given String
+     * - Throws IllegalArgumentException if given String is null
+     */
+    public QuestionNode(String data) {
+        if (String == null) {
+            throw new IllegalArgumentException();
+        } // end of if
+        this.data = data;
+    } // end of constructor
+    
+    /**
+     * Pre:
+     * - Parameters must not be null
+     * Post:
+     * - Constructs a new QuestionNode root storing given data,
+     * and references to a left and right subroot or leaf
+     * - Throws IllegalArgumentException if any parameter is null
+     */
+    public QuestionNode(String data, QuestionNode yes, QuestionNode no) {
+        if (String == null || yes == null || no == null) {
+            throw new IllegalArgumentException();
+        } // end of if
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    } // end of constructor
+    
+    /**
+     * Pre: None
+     * Post:
+     * - Returns the QuestionNode's data
+     */
+    public String getData() {
+        return data;
+    } // end of getData method
+    
+    /**
+     * Pre: None
+     * Post:
+     * - Returns the QuestionNode's left child subroot or leaf
+     */
+    public QuestionNode getLeftNode() {
+        return left;
+    } // end of getLeftNode method
+    
+    /**
+     * Pre: None
+     * Post:
+     * - Returns the QuestionNode's right child subroot or leaf
+     */
+    public QuestionNode getRightNode() {
+        return right;
+    } // end of getRightNode method
+    
+    /**
+     * Pre: String parameter must not be null
+     * Post:
+     * - Sets the QuestionNode's data to given String
+     * - Throws IllegalArgumentException if given String is null
+     */
+    public void setData(String data) {
+        if (data == null) {
+            throw new IllegalArgumentException();
+        } // end of if
+        this.data = data;
+    } // end of setData method
+    
+    /**
+     * Pre: QuestionNode parameter must not be null
+     * Post:
+     * - Sets the QuestionNode's left child to given subroot or leaf
+     * - Throws IllegalArgumentException if given QuestionNode is null
+     */
+    public void setLeftNode(QuestionNode left) {
+        if (left == null) {
+            throw new IllegalArgumentException();
+        } // end of if
+        this.left = left;
+    } // end of setLeftNode method
+    
+    /**
+     * Pre: QuestionNode parameter must not be null
+     * Post:
+     * - Sets the QuestionNode's right child to given subroot or leaf
+     * - Throws IllegalArgumentException if given QuestionNode is null
+     */
+    public void setRightNode(QuestionNode right) {
+        if (right == null) {
+            throw new IllegalArgumentException();
+        } // end of if
+        this.right = right;
+    } // end of setRightNode method
+} // end of QuestionNode class
